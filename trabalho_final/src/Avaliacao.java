a8acc03e70f666539474c9648be913710e1d38ea
Felipe Flores <fsflores97@gmail.com>
2025-12-01 11:14:09 -0300

    implementando classe Avaliacao

--- trabalho_final/src/Avaliacao.java
+++ trabalho_final/src/Avaliacao.java
@@ -1,3 +1,25 @@
 public class Avaliacao {
 
+    private Obra obra;
+    private int nota;
+    private String comentario;
+
+    public Avaliacao(Obra obra, int nota, String comentario) {
+        this.obra = obra;
+        this.nota = nota;
+        this.comentario = comentario;
+    }
+
+    public Obra getObra() {
+        return obra;
+    }
+
+    public int getNota() {
+        return nota;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
 }
+

cf0958b299d38f47f1dde830827d0de271ceb6b4
lucac <lucacallegari71@gmail.com>
2025-11-30 23:31:42 -0300

    Adição de capitulos e criação de novos capitulos

--- trabalho_final/src/Obra.java
+++ trabalho_final/src/Obra.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public abstract class Obra {
 	private String titulo;
@@ -8,6 +10,7 @@ public abstract class Obra {
 	private String sinopse;
 	private double avaliacao;
 	private List<Genero> generos = new ArrayList<>();
+	private Map<Integer, Capitulo> capitulos = new HashMap<>();
 
 	public Obra(String titulo, String autor, int capitulos, String sinopse, double avaliacao) {
 		this.titulo = titulo;
@@ -51,4 +54,36 @@ public abstract class Obra {
 	}
 
 	public abstract String getTipo();
+
+	public Capitulo criarCapitulo(String tituloCap, int numPaginas) {
+        try {
+            if (tituloCap == null) {
+                throw new IllegalArgumentException("Título do capítulo inválido.");
+            }
+            if (numPaginas <= 0) {
+                throw new IllegalArgumentException("Número de páginas deve ser maior que zero.");
+            }
+            Capitulo c = new Capitulo(tituloCap, numPaginas, this);
+            capitulos.put(capitulos.size() + 1, c);
+            return c;
+        } catch (IllegalArgumentException e) {
+            System.err.println("Erro ao criar capítulo: " + e.getMessage());
+            return null;
+        } catch (Exception e) {
+            System.err.println("Erro inesperado ao criar capítulo: " + e.getMessage());
+            return null;
+        }
+    }
+
+	public Capitulo getCapitulo(Integer numero) {
+        if (numero == null) 
+			return null;
+        return capitulos.get(numero);
+    }
+
+	public Capitulo removerCapitulo(Integer numero) {
+        if (numero == null) 
+			return null;
+        return capitulos.remove(numero);
+    }
 }
